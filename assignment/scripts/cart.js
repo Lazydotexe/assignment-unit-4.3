42f420e9fac56457f271ae9ae909ae5cabdf494a
LazyDotExe <papaporo@proton.me>
2023-06-29 22:10:30 -0500

    completed the cart assignment, there is an issue with the test.js file adding elements to arrays built in cart.js

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,6 +1,46 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+
+function addItem(add) {
+    basket.push(add);
+    return true;
+
+}
+
+console.log('Adding Donuts ', addItem('Donuts'));
+console.log('Adding fruit ', addItem('fruit'));
+console.log('Adding veggies ', addItem('veggies'));
+console.log('My basket includes: ', basket);
+
+
+//basket is including 'Kale' and 'Spinach' from the test.js file
+
+
+
+
+function listItems(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+    }
+    
+}
+
+
+//........................................................
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
